d8c1419a2b1be33a479e230c24f9722f8c588344
SuryaShankar94 <bsuryashankar@stratapps.com>
2025-03-11 19:12:12 +0530

    Add files via upload

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,16 +8,22 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        //features = {"src\\Feature\\EmailToCampaign.feature","src\\Feature\\VideoToCampaign.feature"},
-        features = "src\\Feature\\",
+        features = {
+               // "src\\Feature\\VideoToCampaign.feature",
+               // "src\\Feature\\EmailToCampaign.feature",
+               // "src\\Feature\\Opportunities.feature",
+               // "src\\Feature\\OpportunitiesDeal.feature"
+                "src\\Feature\\SurveyCampaign.feature"
+        },
+        //features = "src\\Feature\\",
         glue = {"stepDefinition", "base"},
         monochrome = true,
 
         plugin = {"html:target/Latest_Result_Report/Application_UI.html","pretty",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        dryRun = false,
-        //tags = "@Functional",
-        tags = "@Test_OpportunitiesManageLeads"
+        dryRun = false
+        //tags = "@Test_SurveyToCampaign",
+        //tags = "@Test_OpportunitiesManageLeads"
         //tags = "@Test_VideotoCampaign"
         //tags = "@Test_EmailToCampaign or @SaveEmailCampaign or @ScheduleEmailCampaign"
         //tags = "@Test_EmailToCampaign"
